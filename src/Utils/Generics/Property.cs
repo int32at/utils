59e6ad0333da8a557e2dcf3e42f78a8308968b9c
int32at <admin@andreas-scharf.at>
2014-09-22 17:08:10 +0200

    added generic property

---
+++ src/Utils/Generics/Property.cs
@@ -0,0 +1,17 @@
+﻿namespace int32.Utils.Generics
+{
+    public class Property<T>
+    {
+        public T Value { get; set; }
+
+        public static implicit operator T(Property<T> value)
+        {
+            return value.Value;
+        }
+
+        public static implicit operator Property<T>(T value)
+        {
+            return new Property<T> { Value = value };
+        }
+    }
+}
