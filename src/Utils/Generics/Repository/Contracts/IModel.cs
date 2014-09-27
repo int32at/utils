e8de24a7fbe7deafaeee18e78934b95a5cd2ffb7
int32at <admin@andreas-scharf.at>
2014-09-27 19:10:43 +0200

    removed unused method

--- src/Utils/Generics/Repository/Contracts/IModel.cs
+++ src/Utils/Generics/Repository/Contracts/IModel.cs
@@ -1,18 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-
-namespace int32.Utils.Generics.Repository.Contracts
+﻿namespace int32.Utils.Generics.Repository.Contracts
 {
     public interface IModel
     {
         string Return();
-    }
-
-    public static class ModelExtensions
-    {
-        public static string Return<T>(this IEnumerable<T> models, Func<IEnumerable<T>, string> action) where T : IModel
-        {
-            return action(models);
-        }
     }
 }
