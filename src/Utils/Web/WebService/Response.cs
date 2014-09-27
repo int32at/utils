9b850d6438074851efda3932bc51354206d395d4
int32at <admin@andreas-scharf.at>
2014-09-27 14:58:33 +0200

    added web service responses

---
+++ src/Utils/Web/WebService/Response.cs
@@ -0,0 +1,19 @@
+﻿using int32.Utils.Web.WebService.Contracts;
+
+namespace int32.Utils.Web.WebService
+{
+    public enum StatusCode
+    {
+        Ok,
+        Error
+    }
+
+    public class Response<T> : IResponse<T>
+    {
+        public StatusCode Status { get; set; }
+        public string Error { get; set; }
+        public T Result { get; set; }
+    }
+
+    public class Response : Response<object> { }
+}
---
+++ src/Utils/Web/WebService/WebServiceHandler.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using int32.Utils.Extensions;
+using int32.Utils.Web.WebService.Contracts;
+
+namespace int32.Utils.Web.WebService
+{
+    public static class WebServiceHandler
+    {
+        public static Response<T> Handle<T>(Func<T> action)
+        {
+            var response = new Response<T>();
+
+            try
+            {
+                response.Result = action();
+
+                if (!string.IsNullOrEmpty(response.Error))
+                    SetFailure(response, response.Error);
+            }
+            catch (Exception ex)
+            {
+                SetFailure(response, ex.Message);
+            }
+
+            return response;
+        }
+
+        public static Response<T> Handle<T>(Func<Response<T>, T> action)
+        {
+            var response = new Response<T>();
+
+            try
+            {
+                response.Result = action(response);
+
+                if (!string.IsNullOrEmpty(response.Error))
+                    SetFailure(response, response.Error);
+            }
+            catch (Exception ex)
+            {
+                SetFailure(response, ex.Message);
+            }
+
+            return response;
+        }
+
+        private static void SetFailure<T>(IResponse<T> response, string message)
+        {
+            response.Status = StatusCode.Error;
+            response.Error = message;
+            response.Result = default(T);
+        }
+    }
+}
