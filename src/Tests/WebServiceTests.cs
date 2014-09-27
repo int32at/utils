16f3f7ab36c0d6161294d53b2b62d5fcd1affbb6
int32at <admin@andreas-scharf.at>
2014-09-27 19:18:53 +0200

    added web service tests

---
+++ src/Tests/WebServiceTests.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.IO;
+using int32.Utils.Logger;
+using int32.Utils.Logger.Contracts;
+using int32.Utils.Logger.Loggers;
+using int32.Utils.Web.WebService;
+using NUnit.Framework;
+using Tests.Samples;
+
+namespace Tests
+{
+    [TestFixture]
+    public class WebServiceTests
+    {
+        [TestCase]
+        public void WebService_Handler_HandleSample()
+        {
+            var response = WebServiceHandler.Handle(GetData);
+
+            Assert.AreEqual(StatusCode.Ok, response.Status);
+            Assert.IsNullOrEmpty(response.Error);
+            Assert.AreEqual(23, response.Result.Age);
+        }
+
+        [TestCase]
+        public void WebService_Handler_HandleNull()
+        {
+            var response = WebServiceHandler.Handle<SampleModel>(GetDataException);
+
+            Assert.AreEqual(StatusCode.Error, response.Status);
+            Assert.AreEqual("Test", response.Error);
+        }
+
+        [TestCase]
+        public void WebService_Handler_HandleException()
+        {
+            var response = WebServiceHandler.Handle(GetDataExceptionDirect);
+
+            Assert.AreEqual(StatusCode.Error, response.Status);
+            Assert.AreEqual("TestDirect", response.Error);
+            Assert.IsNull(response.Result);
+        }
+
+        private SampleModel GetData()
+        {
+            return new SampleModel { Age = 23 };
+        }
+
+        private SampleModel GetDataException(Response<SampleModel> response)
+        {
+            try
+            {
+                throw new Exception("Test");
+            }
+            catch (Exception ex)
+            {
+                response.Error = ex.Message;
+            }
+
+            return null;
+        }
+
+        private SampleModel GetDataExceptionDirect()
+        {
+            throw new Exception("TestDirect");
+        }
+    }
+}
