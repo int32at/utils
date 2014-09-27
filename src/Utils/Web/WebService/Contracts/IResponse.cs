1d744a6a7871ea3f3a673e0141ac948102b8aa98
int32at <admin@andreas-scharf.at>
2014-09-27 14:58:33 +0200

    removed unused usings

---
+++ src/Utils/Web/WebService/Contracts/IResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace int32.Utils.Web.WebService.Contracts
+{
+    public interface IResponse<T>
+    {
+        StatusCode Status { get; set; }
+        string Error { get; set; }
+        T Result { get; set; }
+    }
+}
