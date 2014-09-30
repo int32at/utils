721edcbf8e0e6d13d94ebfa3b089833ca748897d
int32at <admin@andreas-scharf.at>
2014-09-30 13:25:20 +0200

    added serializable exception + extension method for exceptions

---
+++ src/Utils/Core/Exceptions/SerializableException.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace int32.Utils.Core.Exceptions
+{
+    [Serializable]
+    public class SerializableException
+    {
+        public DateTime At { get; set; }
+        public string Message { get; set; }
+        public string StackTrace { get; set; }
+
+        public SerializableException()
+        {
+            At = DateTime.Now;
+        }
+
+        public SerializableException(Exception ex)
+            : this()
+        {
+            Message = ex.Message;
+            StackTrace = ex.StackTrace;
+        }
+
+        public SerializableException(string message)
+            : this()
+        {
+            Message = message;
+        }
+
+        public override string ToString()
+        {
+            return Message + StackTrace;
+        }
+    }
+}
---
+++ src/Utils/Core/Extensions/ExceptionExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using int32.Utils.Core.Exceptions;
+
+namespace int32.Utils.Core.Extensions
+{
+    public static class ExceptionExtensions
+    {
+        public static SerializableException ToSerializable(this Exception ex)
+        {
+            return new SerializableException(ex);
+        }
+    }
+}
