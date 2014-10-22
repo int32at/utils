2f1ef3bada8773bc41682543191e520e0107d0d6
Andreas Scharf <admin@andreas-scharf.at>
2014-10-22 17:06:19 +0200

    added basic web server; with view support and model rendering

---
+++ src/Utils40/Web/WebServer/WebController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace int32.Utils.Web.WebServer.Controller
+{
+    public abstract class WebController
+    {
+        public Func<dynamic, dynamic> Get { get; set; }
+
+        public string Path { get; set; }
+
+        protected WebController(string viewName)
+        {
+            Path = viewName;
+        }
+    }
+}
